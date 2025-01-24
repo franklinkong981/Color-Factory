8bfe08150f809ebeca0f24b55b48ff63cf061663
Franklin Kong <franklinkong981@gmail.com>
2025-01-24 00:22:52 -0800

    Completed Routes layout in App component

--- color-factory/src/App.jsx
+++ color-factory/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {Routes, Navigate, Route} from "react-router-dom";
+
+import ColorList from "./ColorList.jsx";
+import NewColorForm from "./NewColorForm.jsx";
+import ColorView from "./ColorView.jsx";
+
 import './App.css';
 
 function App() {
 
   return (
-    
+    <div className="App">
+      <Routes>
+        <Route path="/colors" element={<ColorList />} />
+        <Route path="/colors/new" element={<NewColorForm />} />
+        <Route path="/colors/:color" element={<ColorView />} />
+        <Route path="*" element={<Navigate to="/colors" />} />
+      </Routes>
+    </div>
   );
 }
 
