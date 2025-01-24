1ae30ee4a35a96970b084f18afa6e68fa83330e1
Franklin Kong <franklinkong981@gmail.com>
2025-01-24 00:53:30 -0800

    Added colors list as state and form submission function to App component

--- color-factory/src/App.jsx
+++ color-factory/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Routes, Navigate, Route} from "react-router-dom";
 
 import ColorList from "./ColorList.jsx";
@@ -8,12 +8,17 @@ import ColorView from "./ColorView.jsx";
 import './App.css';
 
 function App() {
+  const [colors, setColors] = useState([]);
+
+  const addNewColor = (newColor) => {
+    setColors([newColor, ...colors]);
+  }
 
   return (
     <div className="App">
       <Routes>
-        <Route path="/colors" element={<ColorList />} />
-        <Route path="/colors/new" element={<NewColorForm />} />
+        <Route path="/colors" element={<ColorList colors={colors}/>} />
+        <Route path="/colors/new" element={<NewColorForm addNewColor={addNewColor}/>} />
         <Route path="/colors/:color" element={<ColorView />} />
         <Route path="*" element={<Navigate to="/colors" />} />
       </Routes>
---
+++ color-factory/src/NewColorForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewColorForm = ({addNewColor}) => {
+
+};
+
+export default NewColorForm;
