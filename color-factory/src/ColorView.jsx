b8f4d0818d91ef49010cbfecb83a856a3933aa6d
Franklin Kong <franklinkong981@gmail.com>
2025-01-24 10:07:15 -0800

    Finished ColorView component

--- color-factory/src/ColorView.jsx
+++ color-factory/src/ColorView.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ColorView = ({color}) => {
-
+  return (
+    <div className="ColorView" style={{backgroundColor: color.hex}}>
+      <h1 classname="ColorView-description">This is {color.name}, hex value: {color.hex}</h1>
+    </div>
+  );
 };
 
 export default ColorView;
